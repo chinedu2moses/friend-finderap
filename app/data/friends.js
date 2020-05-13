4ada32812ac8ee013edbf1f9cc7e4d91bf41145f
chinedu <60300002+chinedu2moses@users.noreply.github.com>
2020-05-12 23:59:04 -0500

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+var friends = [
+    {
+      name: "Haifa Wehbe",
+      photo:
+        "https://www.specialarabia.com/uploads/articles_uploads/Haifa-Wehbe.jpg",
+      scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 4]
+    },
+    {
+      name: "Joelle Mardinian",
+      photo:
+        "https://www.specialarabia.com/uploads/articles_uploads/JOELLE-MARDINIAN.jpg",
+      scores: [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+    },
+    {
+      name: "Anthony Joshua",
+      photo:
+        "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
+      scores: [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+    },
+    {
+      name: "Henry Cavill",
+      photo:
+        "https://www.specialarabia.com/uploads/articles_uploads/Karen_Wazen.jpg",
+      scores: [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+    },
+    {
+      name: "Jennifer Lawrence",
+      photo:
+        "https://i.insider.com/5a43c49efcdf1e62128b4893?width=700&format=jpeg&auto=webp",
+      scores: [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+    },
+    {
+      name: "Harry Styles",
+      photo:
+        "https://i.insider.com/5a446ec0fcdf1e58128b48d5?width=700&format=jpeg&auto=webp",
+      scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
+    },
+    {
+      name: "Christopher Polk",
+      photo:
+        "http://personal.lse.ac.uk/polk/default_files/image002.jpg",
+      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+    },
+    {
+      name: "Robert Downey",
+      photo:
+      "https://www.fillgap.news/wp-content/uploads/2020/04/robert-downey-jr-fillgapnews-696x435.jpg",
+      scores: [3, 4, 5, 4, 3, 2, 4, 3, 2, 3,]
+    },
+    {
+      name: 'henry',
+      photo: 'https://www.fillgap.news/wp-content/uploads/2020/04/Henry-Cavill-fillgapnews-696x464.jpg',
+      scores: [ 5, 3, 4, 1, 4, 5, 2, 4, 4, 4 ]
+    },
+  ];
+  module.exports = friends;
